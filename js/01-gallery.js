02f38dbc85736199e43aaaa1fd431897637dbf99
Doluvit <doluvit@gmail.com>
2023-03-28 16:37:00 +0300

    JS-HW-7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+const galleryElements = makeGallery(galleryItems);
+galleryRef.insertAdjacentHTML("beforeend", galleryElements);
+galleryRef.addEventListener("click", onClickModalOpen);
+let instance = "";
+
+function makeGallery(elements) {
+  return elements
+    .map(
+      ({ original, preview, description }) => `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+        </a></li>`
+    )
+    .join("");
+}
+
+function onClickModalOpen(event) {
+  const { target } = event;
+  event.preventDefault();
+  if (target.nodeName !== "IMG") {
+    return;
+  }
+  instance = basicLightbox.create(`
+<img
+src="${target.dataset.source}"
+alt="${target.alt}"
+/>
+`);
+  instance.show();
+  window.addEventListener("keydown", onEscPressModalClose);
+}
+
+function onEscPressModalClose(event) {
+  if (event.code === "Escape") {
+    instance.close();
+    window.removeEventListener("keydown", onEscPressModalClose);
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryRef = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const makeGallery = (elements) => {
+  return elements
+    .map(
+      (element) => `<li class="gallery__item">
+    <a class="gallery__link" href="${element.original}">
+    <img
+    class="gallery__image"
+    src="${element.preview}"
+    alt="${element.description}"
+    />
+    </a></li>`
+    )
+    .join("");
+};
+const galleryElements = makeGallery(galleryItems);
+galleryRef.insertAdjacentHTML("beforeend", galleryElements);
+
+galleryRef.addEventListener("click", onClick);
+
+function onClick(event) {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  event.preventDefault();
+}
+new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
